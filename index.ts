e643c8769775501660be8e6e5d29d4083d7c67e0
Luis Antonio <contact.ruisuantonio@proton.me>
2025-04-25 10:19:17 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { generate as g } from "random-words";
+
+const _ = () => {
+  const a = document.getElementById('msBingFrame') as HTMLIFrameElement;
+  let b = 0;
+
+  const c = () => {
+    const d = Math.floor(Math.random() * 10) + 1;
+    const e = g(d);
+    
+    if (Array.isArray(e)) {
+      return e.join(' ');
+    } else {
+      return e;
+    }
+  }
+
+  const f = () => {
+    const g_ = c();
+    a.src = atob('aHR0cHM6Ly93d3cuYmluZy5jb20vc2VhcmNoP3E9') + g_ + atob('JlBDPVUzMTYmZm9ybT1BU0RTQko=');
+    const h = Math.floor(Math.random() * 2000) + 8000;
+
+    b++;
+    console.log(atob('V2FpdGluZyBmb3IgNCBtaW51dGVzLi4u'), b, atob('JlBDPVUzMTYmZm9ybT1BU0RTQko='), h, 'ms');
+
+    if (b % 3 === 0) {
+        console.log(atob('V2FpdGluZyBmb3IgNCBtaW51dGVzLi4u'));
+        setTimeout(f, 4 * 60 * 1000); 
+      } else if (b < 35) {
+        setTimeout(f, h);
+      } else {
+        console.log(atob('UmVhY2hlZCAzNSBzZWFyY2hlcy4gU3RvcHBpbmcgYW5kIGNsb3NpbmcgdGhlIHRhYi4uLg=='));
+        window.close(); 
+      }      
+
+  f();
+}
+
+_();
